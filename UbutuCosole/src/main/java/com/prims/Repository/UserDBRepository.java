edb9296adfb3d76035811b18d5c87e4ee2f91708
publicDH <65289736+publicDH@users.noreply.github.com>
2021-03-18 21:47:39 +0900

    Delete UbutuCosole directory

--- UbutuCosole/src/main/java/com/prims/Repository/UserDBRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prims.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserDBRepository extends MongoRepository<User, String> {
-	
-	public User findByid(String id);
-}
